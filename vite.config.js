40a4b90f143d5f9c641e8bfd094fef429c0d9721
GermanHernandez2902 <german.hernandez2902@gmail.com>
2025-04-03 21:01:21 -0500

    Proyecto listo para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./", // Usa rutas relativas en los enlaces generados en el HTML
+  base: "./",       // Para que los assets usen rutas relativas
+  root: ".",        // Asegura que la raíz del proyecto es la carpeta actual
+  build: {
+    outDir: "dist", // Carpeta donde Vite pondrá el resultado final (Vercel usa esto)
+    emptyOutDir: true
+  }
 });
